30482ce222e51e5468a7387493a2847f59291368
Areg Yeghnazar <aregy@are.gy>
2024-09-03 18:41:52 -0700

    theme change in terminal for better readability

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 config.cursor_thickness = 3
 config.font_size=14
-config.color_scheme = 'Harmonic16 Light (base16)'
+--config.color_scheme = 'Catppuccin Latte (Gogh)'
+config.color_scheme = 'Clrs (Gogh)'
 config.hide_tab_bar_if_only_one_tab = true
 return config
